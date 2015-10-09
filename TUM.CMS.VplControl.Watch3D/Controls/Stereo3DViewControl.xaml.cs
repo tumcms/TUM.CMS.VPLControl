427b47ab54bcfd9af942e293a5fff12764f39e61
Cornelius Preidel <cornelius.preidel@googlemail.com>
2015-10-09 10:47:45 +0200

    Stereo3DView added

---
+++ TUM.CMS.VplControl.Watch3D/Controls/Stereo3DViewControl.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using HelixToolkit.Wpf;
+
+namespace TUM.CMS.VplControl.Watch3D.Controls
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Stereo3DViewControl
+    {
+        public Stereo3DViewControl()
+        {
+            InitializeComponent();
+        }
+
+        private void Stereo3DViewControl_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            StereoView3D.InitializeComponent();
+            StereoView3D.SynchronizeStereoModel();
+        }
+    }
+}
